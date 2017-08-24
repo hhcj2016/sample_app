4df7b808eb1b21f4527c37fbb317cc1a00642bec
Cindy <cindyjiang88@163.com>
2017-08-24 19:11:27 +0800

    add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,14 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+                                  foreign_key: "followed_id",
+                                  dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   #返回指定字符串的哈希摘要
   def User.digest(string)
@@ -91,8 +99,31 @@ class User < ApplicationRecord
   #实现动态流原型
   #完整的实现参见第14章
   def feed
-    Micropost.where("user_id = ?", id)
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                    OR user_id = :user_id", user_id: id)
   end
+
+  #关注另一个用户
+  def follow(other_user)
+    #following << other_user
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  #取消关注另一个用户
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  #如果当前用户关注了指定的用户，返回true
+  def following?(other_user)
+   following.include?(other_user)
+  end
+
+
+
+
 
   private
 
@@ -106,5 +137,6 @@ class User < ApplicationRecord
     self.activation_token = User.new_token
     self.activation_digest = User.digest(activation_token)
   end
+
 
 end
--- config/application.rb
+++ config/application.rb
@@ -2,9 +2,13 @@ require_relative 'boot'
 
 require 'rails/all'
 
+require File.expand_path('../boot', __FILE__)
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+
+
 
 module SampleApp
   class Application < Rails::Application
@@ -14,5 +18,9 @@ module SampleApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+
+    #在使用Ajax处理的表单中添加真伪令牌
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
